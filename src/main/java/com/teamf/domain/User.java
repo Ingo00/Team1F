15d2acb473cb30911943c997cd147dfc1ba11ddf
Ingo0 <Ingo0@uw.edu>
2025-03-30 18:33:19 +0000

    Add JUnit test project for Assignment 4 using mvn

--- src/main/java/com/teamf/domain/User.java
+++ src/main/java/com/teamf/domain/User.java
@@ -1,8 +1,8 @@
 package com.teamf.domain;
 
 public class User {
-    private String username;
-    private String email;
+    private final String username;
+    private final String email;
 
     public User(String username, String email) {
         this.username = username;
